aedede3d901256c9a8dcb0a6d142b75b925de690
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-01-05 11:52:23 +0300

    new

--- blog-app/src/views/HomeView.vue
+++ blog-app/src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
   <main>
-    <h1>Welcome</h1>
+    <transition name="fade" mode="out-in">
+      <h1 key="welcome" class="centered-text">Welcome</h1>
+    </transition>
   </main>
 </template>
+
+<style scoped>
+  .centered-text {
+    text-align: center;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
+
